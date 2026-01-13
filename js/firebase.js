d8a846dd6f396f4a1126f3c7e97b17f1ac28de18
soueumeumo <34077748+soueumeumo@users.noreply.github.com>
2026-01-13 15:49:31 -0300

    Update Firebase initialization with new credentials
    Updated Firebase configuration with new project keys.

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,10 @@
 // COLE AQUI SUAS CHAVES DO FIREBASE
-firebase.initializeApp({
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_PROJECT_ID.appspot.com"
+firebase.initializeApp({apiKey: "AIzaSyBAHGI0APt5lulVEO3QlX2HqTmvIi6R3hI",
+  authDomain: "eric-cycles-2020.firebaseapp.com",
+  projectId: "eric-cycles-2020",
+  storageBucket: "eric-cycles-2020.firebasestorage.app",
+  messagingSenderId: "949534257670",
+  appId: "1:949534257670:web:5a2fbf53c5daa5c0c8c987"
 });
 
 const db = firebase.firestore();
