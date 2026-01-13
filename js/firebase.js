94e77d51b5e2e58450e7f87bb7855d363ab81828
soueumeumo <34077748+soueumeumo@users.noreply.github.com>
2026-01-13 17:21:12 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,17 @@
-// COLE AQUI SUAS CHAVES DO FIREBASE
-firebase.initializeApp({
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: "AIzaSyBAHGI0APt5lulVEO3QlX2HqTmvIi6R3hI",
   authDomain: "eric-cycles-2020.firebaseapp.com",
   projectId: "eric-cycles-2020",
   storageBucket: "eric-cycles-2020.firebasestorage.app",
   messagingSenderId: "949534257670",
   appId: "1:949534257670:web:5a2fbf53c5daa5c0c8c987"
-});
+};
 
-const db = firebase.firestore();
-const storage = firebase.storage();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
